dd477a054ef1b015289d19956d2c48d5e64d75e5
gianlucanied <gianluca.nieddu96@gmail.com>
2023-12-05 15:56:42 +0100

    Add: Milestone 1

--- main.js
+++ main.js
@@ -55,3 +55,63 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+/* Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+
+Milestone 1 - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed, prendendo le informazioni che ci servono dall’array di oggetti che già trovate. */
+
+const container = document.getElementById('container');
+
+
+posts.forEach(element => {
+    const {id, content, media, author, likes, created} = element;
+
+    const mioPost = `
+    <div class="post">
+    <div class="post__header">
+      <div class="post-meta">
+        <div class="post-meta__icon">
+          <img
+            class="profile-pic"
+            src="${author.image}"
+            alt="Phil Mangione"
+          />
+        </div>
+        <div class="post-meta__data">
+          <div class="post-meta__author">${author.name}</div>
+          <div class="post-meta__time">${created}</div>
+        </div>
+      </div>
+    </div>
+    <div class="post__text">
+    ${content}
+    </div>
+    <div class="post__image">
+      <img src="${media}" alt="" />
+    </div>
+    <div class="post__footer">
+      <div class="likes js-likes">
+        <div class="likes__cta">
+          <a class="like-button js-like-button" href="#" data-postid="1">
+            <i
+              class="like-button__icon fas fa-thumbs-up"
+              aria-hidden="true"
+            ></i>
+            <span class="like-button__label">Mi Piace</span>
+          </a>
+        </div>
+        <div class="likes__counter">
+          Piace a
+          <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+
+
+    container.innerHTML += mioPost;
+
+})
+
+
